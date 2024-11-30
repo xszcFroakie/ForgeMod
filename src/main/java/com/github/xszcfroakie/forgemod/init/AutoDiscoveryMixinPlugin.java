f4e4ab0c78a79f9e92e1545543f770318e1a57c4
GitHub Action <action@github.com>
2024-11-30 14:10:38 +0000

    Initialize template repository proper
    Set package name to com.github.xszcfroakie.forgemod
    Set project name to ForgeMod
    Set modid to forgemod

--- src/main/java/com/github/xszcfroakie/forgemod/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/xszcfroakie/forgemod/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.xszcfroakie.forgemod.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
